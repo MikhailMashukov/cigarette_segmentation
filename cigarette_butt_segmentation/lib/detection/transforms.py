4c43fef8627786fd96161606359fab1d3f20a27d
Mikhail Mashukov <mashukovm@mail.ru>
2020-10-05 11:33:02 +0700

    Implement copying cigarette many times

--- cigarette_butt_segmentation/lib/detection/transforms.py
+++ cigarette_butt_segmentation/lib/detection/transforms.py
@@ -48,11 +48,18 @@ class RandomHorizontalFlip(object):
                 target["keypoints"] = keypoints
         return image, target
 
+def debugPrint(str, printToConsole=False):
+    with open('debug.log', 'a') as file:
+        file.write(str + '\n')
+    if printToConsole:
+        print(str)
+
 class RandomMaskedObjectCopy(object):
-    def __init__(self):
+    def __init__(self, max_copy_count=1):
+        self.max_copy_count = max_copy_count
         pass
 
-    def __call__(self, image, target):
+    def __call__1(self, image, target):
         height, width = image.size[-2:]
         if target["boxes"].shape[0] == 0:
             return image, target
@@ -94,14 +101,14 @@ class RandomMaskedObjectCopy(object):
                       bbox[0] : bbox[2] + 1, :][obj_mask > 0, :]
             # plt.imshow(image)
             if 0:
-                target["boxes"] = np.stack([target["boxes"], np.expand_dims(new_bbox, 0)], axis=0)
+                target["boxes"] = np.concatenate([target["boxes"], np.expand_dims(new_bbox, 0)], axis=0)
                 new_full_mask = np.zeros(image.shape[:2], dtype=obj_mask.dtype)
                 new_full_mask[new_bbox[1] : new_bbox[3] + 1, 
                             new_bbox[0] : new_bbox[2] + 1][obj_mask > 0] = 2
                 # print(new_full_mask.shape, target["masks"].shape, 
                 #     np.expand_dims(new_full_mask[new_bbox[1] : new_bbox[3] + 1, 
                 #               new_bbox[0] : new_bbox[2] + 1], 0).shape)
-                target["masks"] = np.stack([target["masks"], np.expand_dims(new_full_mask, 0)], axis=0)
+                target["masks"] = np.concatenate([target["masks"], np.expand_dims(new_full_mask, 0)], axis=0)
             else:
                 num_objs = target["boxes"].shape[0]
                 cur_masks = np.array(target['masks'])
@@ -121,7 +128,87 @@ class RandomMaskedObjectCopy(object):
                
             # plt.show()
             return image, target
+
+    def __call__(self, image, target):
+        height, width = image.size[-2:]
+        if target["boxes"].shape[0] == 0:
+            return image, target
+        bboxes = np.array(target["boxes"] + 0.01, dtype=int)  
+        bbox = bboxes[0]
+            # This will be source bounding box during the entire method
+        # print(bbox)
+        cur_bbox_width = int(bbox[2] - bbox[0])
+        cur_bbox_height = int(bbox[3] - bbox[1])
+        num_objs = bboxes.shape[0]
+        
+        obj_mask = np.array(target['masks'][0][bbox[1] : bbox[3] + 1, bbox[0] : bbox[2] + 1])
+            # Bboxes in our dataset has format (left x, top y, right x, bottom y)
+            # and despite typical ranges convention, right and bottom pixels are included. So +1
+        # plt.imshow(obj_mask)
+        pixels_coords = np.where(obj_mask > 0)
+
+        # Gathering potential array of masks with 1s, 2s and so on back into one matrix
+        cur_masks = np.array(target['masks'])
+        # print('cur', np.multiply(cur_masks[0], cur_masks[0]).shape)
+        mask = cur_masks[0]
+        assert num_objs <= 1 or (num_objs > 1 and \
+                np.sum(np.multiply(cur_masks[0], cur_masks[1])) == 0)
+            # Simplified condition that objects don't intersect. Actually usually 
+            # there will be 1 object
+        full_mask = np.sum(cur_masks, axis=0)
+        # print('new_mask', new_mask.min(), new_mask.max(), new_mask)
+
+        max_retry_count = 5
+        image = np.array(image)
+        for new_obj_ind in range(self.max_copy_count):
+            retry_ind = 1
+            while retry_ind <= max_retry_count:
+                new_coords = (random.randrange(width - cur_bbox_width),
+                              random.randrange(height - cur_bbox_height))
+                new_bbox = np.array([new_coords[0], new_coords[1], 
+                                     new_coords[0] + cur_bbox_width, new_coords[1] + cur_bbox_height])
+                intersect_found = False   
+                for obj_ind in range(num_objs):
+                    bbox2 = bboxes[obj_ind]                        
+                    lt = np.maximum(bbox2[:2], new_bbox[:2])
+                    rb = np.minimum(bbox2[2:], new_bbox[2:]) 
+                    wh = (rb - lt)
+                    # inter = wh[0] * wh[1]
+                    # print('%s - %s intersection: %s' % (str(bbox2), str(new_bbox), str(wh)))
+                    if wh[0] > 0 and wh[1] > 0:
+                        intersect_found = True
+                        debugPrint('New obj %d, retry %d: %s - %s (%d)' % \
+                              (new_obj_ind + 1, retry_ind, str(new_bbox), str(bbox2), obj_ind))
+                        break
+                if not intersect_found:
+                    break
+                else:
+                    retry_ind += 1
+            if intersect_found:
+                break
+
+            # Finally free place for new object found
             
+            # import matplotlib.pyplot as plt
+
+            new_pixels_coords = (pixels_coords[0] + new_bbox[1] - bbox[1], 
+                                    pixels_coords[1] + new_bbox[0] - bbox[0])
+            # image[pixels_coords] = 2
+            image[new_bbox[1] : new_bbox[3] + 1, 
+                    new_bbox[0] : new_bbox[2] + 1, :][obj_mask > 0, :] = \
+                image[bbox[1] : bbox[3] + 1, 
+                        bbox[0] : bbox[2] + 1, :][obj_mask > 0, :]
+            # plt.imshow(image)
+            num_objs += 1
+            bboxes = np.concatenate([bboxes, np.expand_dims(new_bbox, 0)], axis=0)
+            full_mask[new_bbox[1] : new_bbox[3] + 1, 
+                      new_bbox[0] : new_bbox[2] + 1][obj_mask > 0] = num_objs
+        target = CigDataset._create_target(full_mask, int(target["image_id"]))
+        # plt.imshow(full_mask)
+               
+        # plt.show()
+        return image, target
+        
 class ToTensor(object):
     def __call__(self, image, target):
         image = F.to_tensor(image)
